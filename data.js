785f02422363e0c00a863b7cf1450f4ab8a3d265
Rashud1 <rashudshrestha12@gmail.com>
2022-04-15 11:25:09 +1000

    successfully connected mongodb with atlas and localhost

--- data.js
+++ data.js
@@ -1,18 +1,20 @@
 const data = {
   products: [
     {
+      _id: '1',
       name: 'Nike Slim Shirt',
       slug: 'nike-slim-shirt',
       category: 'Shirts',
       image: '/images/product-2.jpg',
       price: 120,
-      countInStock: 10,
+      countInStock: 0,
       brand: 'Nike',
       rating: 4.5,
       numReviews: 10,
-      description: 'high quality shirt',
+      description: 'High quality shirt',
     },
     {
+      _id: '2',
       name: 'Adidas Fit Shirt',
       slug: 'adidas-fit-shirt',
       category: 'Shirts',
@@ -25,6 +27,7 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id: '3',
       name: 'Nike Slim Pant',
       slug: 'nike-slim-pant',
       category: 'Pants',
@@ -37,6 +40,7 @@ const data = {
       description: 'high quality product',
     },
     {
+      _id: '4',
       name: 'Adidas Slim Pant',
       slug: 'adidas-slim-pant',
       category: 'Pants',
--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 import express from 'express';
 import data from './data.js';
 import cors from 'cors';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+mongoose
+  .connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log('connected to db');
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+
 //,,now create express app//
 const app = express();
 
@@ -8,6 +21,22 @@ app.use(cors());
 
 app.get('/api/products', (req, res) => {
   res.send(data.products);
+});
+app.get('/api/products/slug/:slug', (req, res) => {
+  const product = data.products.find((x) => x.slug === req.params.slug);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product not found' });
+  }
+});
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product not found' });
+  }
 });
 
 const port = process.env.PORT || 5000;
