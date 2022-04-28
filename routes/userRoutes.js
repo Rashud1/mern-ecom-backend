6f2ee4f3ada2d398975523d0bb36b9706d953047
Rashud1 <rashudshrestha12@gmail.com>
2022-04-28 22:47:28 +1000

    created signup api

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -26,4 +26,24 @@ userRouter.post(
     res.status(401).send({ message: 'Invalid email or password' });
   })
 );
+userRouter.post(
+  '/signup',
+  expressAsyncHandler(async (req, res) => {
+    const newUser = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: bcrypt.hashSync(req.body.password),
+    });
+    console.log(newUser);
+    const user = await newUser.save();
+    console.log(user);
+    res.send({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user),
+    });
+  })
+);
 export default userRouter;
